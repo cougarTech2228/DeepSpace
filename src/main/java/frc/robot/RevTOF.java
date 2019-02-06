20e27ce51b81af669939f4500d6ddb55c6b69690
unknown <cougartechsoftware@gmail.com>
2019-02-06 17:50:47 -0500

    Correct Address

--- src/main/java/frc/robot/RevTOF.java
+++ src/main/java/frc/robot/RevTOF.java
@@ -1,12 +1,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.I2C;
+import java.util.ArrayList;
 
 public class RevTOF {
     I2C device;
 	
 	public RevTOF() {
-		device = new I2C(I2C.Port.kOnboard, 0x54);
+		device = new I2C(I2C.Port.kOnboard, 0x52);
 		
 	}
 	public void read() {
