d6c99e36f7e7563a4b352dc338b39ce989315066
cougartechsoftware <cougartechsoftware@gmail.com>
2019-02-08 18:59:21 -0500

    Changed motors in elevator class to match names we will use

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -5,25 +5,25 @@ package frc.robot;
 
 public class Elevator {
     private DriverIF controls;
-    private Motor front;
-    private Motor back; 
+    private Motor frontlift;
+    private Motor backlift; 
+    private Motor liftdrive;
     private double speed = 0;
     private int encoderCount = 0;
     
 public Elevator(){
 
-
-    front = new Motor (RobotMap.ACTION_MOTOR_1);
-    back = new Motor (RobotMap.ACTION_MOTOR_2);
-
+    frontlift = new Motor (RobotMap.ACTION_MOTOR_1);
+    backlift = new Motor (RobotMap.ACTION_MOTOR_2);
+    liftdrive = new Motor (RobotMap.ACTION_MOTOR_3);
 
 
     }
 
 public void TeleopRaise(){
     if(controls.elevatorUp()){
-        Motor.MoveMotors(encoderCount, speed, front);
-        Motor.MoveMotors(encoderCount, speed, back);
+        Motor.moveMotors(encoderCount, speed, backlift, frontlift);
+        Motor.moveMotors(encoderCount, speed, liftdrive);
         }
     }
 }
